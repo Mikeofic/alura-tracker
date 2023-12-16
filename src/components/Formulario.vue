ba00cb30b1155a8019cb89e964b23ef1b6c2fbb4
MikeOfic <mikeribeirooficial@gmail.com>
2023-12-15 23:43:30 -0300

    feat: added time logic

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -19,10 +19,12 @@
         <div 
           class="is-flex is-align-items-center is-justify-content-space-between">
           <section>
-            <strong>00:00:00</strong>
+            <strong>
+              {{  tempoDecorrido }}
+            </strong>
           </section>
           
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
@@ -32,7 +34,7 @@
             </span>
           </button>
           
-          <button class="button">
+          <button class="button"  @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -52,7 +54,31 @@
   import { defineComponent } from 'vue';
 
   export default defineComponent({
-    name: 'FormTrack'
+    name: 'FormTrack',
+    data () {
+      return {
+        tempoEmSegundos: 0,
+        cronometro: 0
+      }
+    },
+    computed: {
+      tempoDecorrido () : string {
+        return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+      }
+    },
+    methods: {
+
+      iniciar () {
+       this.cronometro = setInterval(() => {
+          this.tempoEmSegundos += 1
+        }, 1000 )
+      },
+
+
+      finalizar () {
+        clearInterval(this.cronometro)
+      }
+    }
   })
 </script>
 
